49c83f80f1430e756150b164328b0a438fa966e7
Owen Pierce <opierce@gmail.com>
2018-12-04 09:18:41 -0800

    Add first project for 2018

---
+++ 2018/aoc-01/aoc-01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  aoc-01
+//
+//  Created by Owen Pierce on 12/3/18.
+//  Copyright © 2018 surfline. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
